78fd70b8eed3a8306f381393e261b3541b432d03
Asit Rohan Dass <asitdass33@gmail.com>
2024-04-28 00:29:56 +0530

    Implemented Three js for the T shirt designing

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import ColourPicker from "./ColourPicker";
+import Tab from "./Tab";
+
+export {
+    CustomButton,
+    AIPicker,
+    ColourPicker,
+    FilePicker,
+    Tab,
+}
